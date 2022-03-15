9fc5415d408ece0e3060e4e527f1cf77a4fdaa76
Ando Avetisyan <andoavetis0127@gmail.com>
2022-03-15 17:19:19 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import UsersNames from './UsersNames';
+
+
+function App() {
+  return (
+    <div className='App'>
+      {UsersNames()}
+    </div>
+  );
+}
+
+export default App;
