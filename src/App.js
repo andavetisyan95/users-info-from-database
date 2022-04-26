7d34bfd45359649321ff5cca40dffc41bbfc3663
Ando Avetisyan <andoavetis0127@gmail.com>
2022-04-26 20:46:15 +0400

    Mixins, exports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from 'react-router';
 //components
-import Users from "./components/Users"
+import Users from "./components/Users";
+import AboutUsers from "./components/AboutUsers"
+//styles
 import './App.css';
 
 
@@ -10,6 +12,7 @@ function App() {
     <div className='App'>
       <Routes>
       <Route path='/' element={<Users />} />
+      <Route path='/aboutUsers' element={<AboutUsers />} />
       </Routes>
     </div>
   );
