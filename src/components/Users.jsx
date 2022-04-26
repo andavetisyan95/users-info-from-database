e977f5e9adc3d5b05a666a38fa736b8214c843ba
Ando Avetisyan <andoavetis0127@gmail.com>
2022-04-26 17:19:04 +0400

    nothing to commit

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,11 +1,13 @@
 //hooks
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 //images
 import unknownImg from "../images/unknown2.jpg"
 //styles
 import styles from "../styles/Users.module.scss"
 
-const URL = "https://jsonplaceholder.typicode.com/users"
+const URL = "https://jsonplaceholder.typicode.com/users";
+
 export default function Users() {
     const [users,setUsers]= useState([]);
 
@@ -24,11 +26,11 @@ export default function Users() {
     <div className={styles.users_div}>
         {
             users.map((user)=>{
-                return <div className={styles.users_div_card} key={user.id}>
-                <img className={styles.users_div_card_img} src={unknownImg } ></img>
-                <p className={styles.users_div_card_pTag}>{user.username}</p>
-            </div>
-            })
+                return  <Link to={"/"} className={styles.users_div_card} key={user.id}>
+                            <img className={styles.users_div_card_img} src={unknownImg } ></img>
+                            <p className={styles.users_div_card_pTag}>{user.username}</p>
+                        </Link>
+                })
         }
     </div>
   )
