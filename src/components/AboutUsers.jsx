e561e3e6bda395218dff40f15294fc97614538a0
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-02 13:08:43 +0400

    Changes

--- src/components/AboutUsers.jsx
+++ src/components/AboutUsers.jsx
@@ -29,20 +29,17 @@ export default function AboutUsers() {
 
   const current_user = usersArray.find(({ id}) => id === +userId)
   
-
-  
-
   return (
     <div className={styles.user}>
       <div className={styles.user_container}>
         <img src={mount} alt="mount" className={styles.user_container_walp} />
         <div className={styles.user_container_about_users}>
           <img src={userPic} alt="user" className={styles.user_container_about_users_img} />
-            <h3 style={{color: "#949425", fontWeight:"800"}}>{current_user.name}</h3> 
-            <h4 style={{color:"#5b9e34"}}>{current_user.username}</h4>
-            <p>Email: <span> {current_user.email} </span></p>
-            <p>Phone: <span>{current_user.phone}</span></p>
-            <p> Websit: <span>{current_user.website}</span></p>
+            <h3 style={{color: "#949425", fontWeight:"800"}}>{current_user?.name}</h3> 
+            <h4 style={{color:"#5b9e34"}}>{current_user?.username}</h4>
+            <p>Email: <span> {current_user?.email} </span></p>
+            <p>Phone: <span>{current_user?.phone}</span></p>
+            <p> Websit: <span>{current_user?.website}</span></p>
           <div className={styles.user_container_btns}>
             <Link to={`/albums/${userId}`}>
               <button className={styles.user_container_btns_1}>Albums</button>
