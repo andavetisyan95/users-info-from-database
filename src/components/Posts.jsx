fa79eb25a51031fe8938519b0c76620986595a83
Ando Avetisyan <andoavetis0127@gmail.com>
2022-05-05 23:05:40 +0400

    Posts: share Context to posts comonent

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,43 +1,47 @@
 //react hooks and helpers
-import React from 'react';
-import { useNavigate, useParams } from 'react-router';
+import React, { useContext } from "react";
+import { useNavigate, useParams } from "react-router";
+//component
+import DarkMoodContext from "../context/DarkMoodContext";
 //styles
 import styles from "../styles/Posts.module.scss";
 
-
 export default function Posts() {
-    const { usId } = useParams();
+  const { usId } = useParams();
+  const [darkMode] = useContext(DarkMoodContext);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    //get request from localStorage
-    const postsStorage = localStorage.getItem("posts");
-    const postsArray = JSON.parse(postsStorage);
+  //get request from localStorage
+  const postsStorage = localStorage.getItem("posts");
+  const postsArray = JSON.parse(postsStorage);
 
-    const current_user = postsArray.filter(({ userId }) => userId === +usId)
+  const current_user = postsArray.filter(({ userId }) => userId === +usId);
 
-   function goBack(){
-        navigate(-1);
-   }
+  function goBack() {
+    navigate(-1);
+  }
 
-    return (
-        <main>
-             <img onClick={goBack} className={styles.back_btn} src="https://img.icons8.com/ios-filled/50/000000/left.png"/>
-            <div className={styles.header}>
-                POSTS
+  return (
+    <main className={`${darkMode ? styles.dark : null}`}>
+      <img
+        onClick={goBack}
+        className={`${styles.back_btn} ${
+          darkMode ? styles.btn_color : styles.back_btn
+        }`}
+        src="https://img.icons8.com/ios-filled/50/000000/left.png"
+      />
+      <div className={styles.header}>POSTS</div>
+      <div className={styles.posts_cont}>
+        {current_user.map((posts) => {
+          return (
+            <div key={posts.id} className={styles.posts_cont_cards}>
+              <h3 className={styles.posts_cont_cards_title}>{posts.title}</h3>
+              <p className={styles.posts_cont_cards_body}>{posts.body}</p>
             </div>
-            <div className={styles.posts_cont}>
-                {
-                    current_user.map((posts)=>{
-                        return(
-                            <div key={posts.id} className={styles.posts_cont_cards}>
-                            <h3 className={styles.posts_cont_cards_title}>{posts.title}</h3>
-                            <p className={styles.posts_cont_cards_body}>{posts.body}</p>
-                        </div>
-                        )
-                    })
-                }
-            </div>
-        </main>
-    )
+          );
+        })}
+      </div>
+    </main>
+  );
 }
